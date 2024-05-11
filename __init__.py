22bec77251fc77effd438b8f6e61633e01fd05d3
xRedCrystalx <xredcrystaledx@gmail.com>
2024-05-11 19:50:19 +0200

    Removed logging

--- __init__.py
+++ __init__.py
@@ -1,18 +1,24 @@
 """
-RedUtils Python library
-──────────────────────────────
+RedUtils Python Library
+────────────────────────────────────────────────
 Simple, general purpose functions in one place! 
+
+Usage:
+
+>>> import red_utils
+>>> red_utils.main_test()
+"All checks passed!"
 
 :copyright: (c) 2024 by xRedCrystalx.
 :license: MIT, see LICENSE for more details.
 """
 
-import logging, sys
+import sys
 
-__title__ = 'RedUtils'
-__author__ = 'xRedCrystalx'
-__license__ = 'MIT'
-__copyright__ = 'Copyright 2024-present xRedCrystalx'
+__title__ = "RedUtils"
+__author__ = "xRedCrystalx"
+__license__ = "MIT"
+__copyright__ = "Copyright 2024-present xRedCrystalx"
 __version__ = "0.0.0-a1"
 
 from src import *
@@ -20,9 +26,6 @@ from src import *
 def check_py_version() -> None | Exception:
     if sys.version_info < (3, 12):
         raise SystemError("Python 3.12 or higher required!")
-
-logging.getLogger(__name__).addHandler(logging.NullHandler())
-
 check_py_version()
 
-del logging, sys
+del sys
