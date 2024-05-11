35fcf8dd0357d4de0690ef8801f8ff73a609dfe6
xRedCrystalx <xredcrystaledx@gmail.com>
2024-05-11 20:25:48 +0200

    Added 2 functions: dict_to_json and dict_merge - other docstring updates

--- src/dicts.py
+++ src/dicts.py
@@ -1,3 +1,9 @@
+"""
+This module provides functions for working with dictionaries and JSON data.
+
+These functions are useful for navigating and manipulating dictionary-like data structures, especially when dealing with nested structures or JSON data.
+"""
+
 import sys, json, io
 sys.dont_write_bytecode = True
 
@@ -31,28 +37,13 @@ def dict_walk(dictionary: dict[SIMPLE_ANY, SIMPLE_ANY], path: str | list[str], _
 
     return current
 
-def json_to_dict(d: bytes | str | bytearray | io.TextIOWrapper, **kwargs) -> dict[str, SIMPLE_ANY]:
-    """
-    Converts JSON data to a Python dictionary.
-
-    ### Parameters:
-    - `d` - JSON data, which can be provided as `bytes`, `str`, `bytearray`, or a `text file`.
-    - `**kwargs` - Additional keyword arguments to pass to the `json.loads` function.
-
-    ### Returns:
-    - A Python dictionary containing the parsed JSON data.
-    """
-    if isinstance(d, io.TextIOWrapper):
-        d = d.read()
-    return json.loads(d, **kwargs)
-
 def value_exist(dictionary: dict[SIMPLE_ANY, SIMPLE_ANY], path: str | list[str], **kwargs) -> bool:
     """
     Checks whether a value exists within a dictionary at the specified path.
 
     ### Parameters:
     - `dictionary` - The dictionary to search within.
-    - `path` The path to the value being checked, specified as a `str` or `list of str`.
+    - `path` The path to the value being checked, specified as a `string` or `list of strings`.
     - `**kwargs` - Additional keyword arguments to pass to the `dicts.dict_walk` function.
 
     ### Returns:
@@ -63,3 +54,46 @@ def value_exist(dictionary: dict[SIMPLE_ANY, SIMPLE_ANY], path: str | list[str],
         return True 
     except:
         return False
+
+def dict_merge(dict1, dict2) -> dict[SIMPLE_ANY, SIMPLE_ANY]:
+    """
+    Merges two dictionaries.
+
+    ### Parameters:
+    - `dict1` - The first dictionary to merge.
+    - `dict2` - The second dictionary to merge.
+
+    ### Returns:
+    - A new `dictionary` containing the merged key-value pairs from both dictionaries.
+    """
+    return dict1 | dict2
+
+
+def json_to_dict(d: bytes | str | bytearray | io.TextIOWrapper, **kwargs) -> dict[str, SIMPLE_ANY]:
+    """
+    Converts JSON data to a Python dictionary.
+
+    ### Parameters:
+    - `d` - JSON data, which can be provided as `bytes`, `string`, `bytearray`, or a `text file`.
+    - `**kwargs` - Additional keyword arguments to pass to the `json.loads` function.
+
+    ### Returns:
+    - A Python dictionary containing the parsed JSON data.
+    """
+    if isinstance(d, io.TextIOWrapper):
+        d = d.read()
+    return json.loads(d, **kwargs)
+
+def dict_to_json(dictionary: dict[SIMPLE_ANY, SIMPLE_ANY], indent: int = 4, **kwargs) -> str:
+    """
+    Converts a dictionary to a JSON string with optional indentation and additional keyword arguments.
+
+    ### Parameters:
+    - `dictionary` - The dictionary to convert to JSON.
+    - `indent` - The number of spaces used for indentation (default is `4`).  *Made for better readablity of JSON, can be set to `None`*
+    - `**kwargs` - Additional keyword arguments to pass to the `json.dumps` function.
+
+    ### Returns:
+    - A JSON string representing the dictionary.
+    """
+    return json.dumps(dictionary, indent=indent, **kwargs)
