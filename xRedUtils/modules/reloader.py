eb5b702c0a228db8e42f7a12c75da3ed50b09991
xRedCrystalx <xredcrystaledx@gmail.com>
2024-06-12 22:42:57 +0200

    Reloading ready

--- xRedUtils/modules/reloader.py
+++ xRedUtils/modules/reloader.py
@@ -5,84 +5,52 @@ __all__ = (
     "load_module", "unload_module", "reload_module"
 )
 
-def resolve_name(name: str, package: str = None) -> str:
-    return importlib.util.resolve_name(name, package)
+def load_module(path: str) -> types.ModuleType:
+    """
+    Imports new python module (in sys.modules) in the current running instance.
+    
+    ## WARNING: Adding a module dynamically can result to errors, crashes, overwrites etc. if not used properly. USE WITH YOUR OWN RISK!
+    
+    ### Parameters:
+    - `path` - Python like path of the module. (using `.` as seperators)
 
-def load_module(name: str, package: str = None) -> types.ModuleType:
-    return importlib.import_module(name, package)
+    ### Returns:
+    - `ModuleType` (like `import x.y.z as r` > `r`) can be used to call module methods, classes, variables...
+    """
+    return importlib.import_module(path, None)
 
-def unload_module(name: str, package: str = None) -> str | None:
+def unload_module(path: str) -> str | None:
     """
     Removes cached module (sys.modules) from the current running instance.
     
     ## WARNING: Removing a module dynamically can result to errors, crashes etc. if not used properly. USE WITH YOUR OWN RISK!
     
     ### Parameters:
-    - `name` - Python like path of the module. (using `.` as seperators)
-    - `package` - 
+    - `path` - Python like path of the module. (using `.` as seperators)
 
     ### Returns:
-    - Python path, if sucessful else `None`
-
+    - Python path, if successful else `None`.
     """
-    path: str = resolve_name(name, package)
     if path in sys.modules:
         try:
             del sys.modules[path]
             return path
         except: pass
 
-def reload_module(name: str, package: str = None, ignore_not_loaded: bool = False) -> types.ModuleType | None:
-    # removing cached module from system
-    if not unload_module(name, package) and not ignore_not_loaded:
+def reload_module(path: str, ignore_not_loaded: bool = False) -> types.ModuleType | None:
+    """
+    Reloads cached module (sys.modules) in the current running instance with new version.
+    
+    ## WARNING: Reloading a module dynamically can result to errors, crashes, overwrites etc. if not used properly. USE WITH YOUR OWN RISK!
+    
+    ### Parameters:
+    - `path` - Python like path of the module. (using `.` as seperators)
+    - `ignore_not_loaded` - 
+
+    ### Returns:
+    - `ModuleType` of reloaded version, if successful else `None`.
+    """
+    if not unload_module(path) and not ignore_not_loaded:
         raise ImportWarning("Module not loaded. Use `ignore_not_loaded = True` to bypass this warning or load the module using `load_module`")
 
-    return load_module(name, package)
-
-
-"""
-
-
-def levenshtein_distance(string1: str, string2: str) -> int:    
-    
-    # switch (first argument must be longer than second)
-    if len(string1) < len(string2):
-        return levenshtein_distance(string2, string1)
-
-    # if second string is empty, means len(string1) instertions
-    if len(string2) == 0:
-        return len(string1)
-
-    previous_row: list[int] = range(len(string2) + 1)
-    
-    for index1, char1 in enumerate(string1):
-        current_row: list[int] = [index1+1]
-
-        for index2, char2 in enumerate(string2):
-            insertions: int = previous_row[index2+1] + 1
-            deletions: int = current_row[index2] + 1
-            substitutions: int = previous_row[index2] + (char1 != char2)
-            current_row.append(min(insertions, deletions, substitutions))
-    
-        previous_row: list[int] = current_row
-    
-    return previous_row[-1]
-
-
-def similarity_percentage(s1: str, s2: str) -> float:
-    max_len = max(len(s1), len(s2))
-    if max_len == 0:
-        return 100.0  # Both strings are empty, hence 100% similar
-    distance = levenshtein_distance(s1, s2)
-    print(distance)
-
-    similarity = ((max_len - distance) / max_len) * 100
-    return similarity
-
-# Example usage
-if __name__ == "__main__":
-    s1 = "Free 18+ Teen Porn + Latina Nudes + Onlyfans Leaks + Hot Girls + Hot pussys +E-Girls Porn + Hentai Porn  🍑🔞@everyone "
-    s2 = "@everyone 🔞 Free 18+ Teen Porn, Onlyfans Leaks, Hot Girls, E-Girls Porn and Hentai Porn 🍑"
-    similarity = similarity_percentage(s1, s2)
-    print(f"Similarity between\n'{s1}' and\n'{s2}'\nis {similarity:.2f}%")
-"""
+    return load_module(path)
