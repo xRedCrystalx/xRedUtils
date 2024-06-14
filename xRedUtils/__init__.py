72365ebc6b56978a37681895a1377ed290ad54df
xRedCrystalx <xredcrystaledx@gmail.com>
2024-06-14 22:15:47 +0200

    patch version upd

--- xRedUtils/__init__.py
+++ xRedUtils/__init__.py
@@ -21,7 +21,7 @@ __title__ = "RedUtils"
 __author__ = "xRedCrystalx"
 __license__ = "MIT"
 __copyright__ = "Copyright 2024-present xRedCrystalx"
-__version__ = "1.0.5"
+__version__ = "1.0.5-1"
 
 def check_py_version() -> None | SystemError:
     if sys.version_info < (3, 12):
