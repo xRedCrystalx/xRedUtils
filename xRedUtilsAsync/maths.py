040a23808aeeb417f2ba44408853b32bfbb8a536
xRedCrystalx <xredcrystaledx@gmail.com>
2024-12-26 23:01:12 +0100

    added sma, ema and isPrime

--- xRedUtilsAsync/maths.py
+++ xRedUtilsAsync/maths.py
@@ -5,6 +5,9 @@ This module provides async mathematics functions.
 - `root` - Computes the n-th root of a given value.
 - `percentage_difference` - Calculates the percentage difference between two numbers.
 - `value_from_percentage` - Calculates the value corresponding to a given percentage of a total.
+- `sma` - Calculate the Simple Moving Average (SMA).
+- `ema` - Calculate the Exponential Moving Average (EMA).
+- `isPrime` - Check if a number is prime.
 
 ### Usage:
 ```py
@@ -23,7 +26,7 @@ from .type_hints import NUMBER
 from .errors import InvalidRootError
 
 __all__: tuple[str, ...] = (
-    "root", "percentage_difference", "value_from_percentage"
+    "root", "percentage_difference", "value_from_percentage", "sma", "ema", "isPrime"
 )
 
 async def root(value: NUMBER, n: int) -> NUMBER:
@@ -79,3 +82,72 @@ async def value_from_percentage(total: NUMBER, percentage: NUMBER) -> NUMBER:
     - The value as `float` that corresponds to the given percentage of the total.
     """
     return (percentage / 100) * total
+
+async def sma(data: list[NUMBER], period: NUMBER) -> list[NUMBER]:
+    """
+    Calculate the Simple Moving Average (SMA).
+    
+    ### Parameters:
+    - `data` - List of numerical values
+    - `period` - Number of periods to calculate the SMA
+
+    ### Returns:
+    - `List` of SMA (numerical) values.
+
+    ### Raises:
+    - `ValueError` if `period > len(data)`
+    """
+    if len(data) < period:
+        raise ValueError("Data length cannot be less than the SMA period.")
+    
+    return [sum( data[i:i+period] ) / period for i in range(len(data) - period + 1)]
+
+async def ema(data: list[NUMBER], period: NUMBER) -> list[NUMBER]:
+    """
+    Calculate the Exponential Moving Average (EMA).
+    
+    ### Parameters:
+    - `data` - List of numerical values
+    - `period` - Number of periods to calculate the EMA
+
+    ### Returns:
+    - `List` of EMA (numerical) values.
+
+    ### Raises:
+    - `ValueError` if `period > len(data)`
+    """
+    if len(data) < period:
+        raise ValueError("Data length cannot be less than the EMA period.")
+    
+    ema: list[NUMBER] = [ (sum(data[:period]) / period) ] # Initial SMA to be able to calculate EMA    
+    multiplier: NUMBER = 2 / (period + 1)
+
+    for i in range(period, len(data)):
+        ema.append((data[i] - ema[-1]) * multiplier + ema[-1])
+    return ema
+
+async def isPrime(n) -> bool:
+    """
+    Check if a number is prime.
+    
+    ### Parameters:
+    - `n` - Integer to check.
+
+    ### Returns:
+    - Boolean if n is prime or not.
+    """
+    if n <= 1:
+        return False  # 0 and 1 are not prime numbers
+    
+    if n <= 3:
+        return True  # 2 and 3 are prime numbers
+    
+    if n % 2 == 0 or n % 3 == 0:
+        return False  # Eliminate multiples of 2 and 3
+    
+    # Check divisors from 5 to sqrt(n), skipping even numbers
+    for i in range(5, await root(n, 2) + 1, 6):
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+    
+    return True
