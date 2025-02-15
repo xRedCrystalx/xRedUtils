d100286e4d9012e50b85545f757a2d206598258c
xRedCrystalx <xredcrystaledx@gmail.com>
2025-02-15 14:59:34 +0100

    Moved TIME_UNITS from times.py

--- xRedUtils/general.py
+++ xRedUtils/general.py
@@ -1,6 +1,9 @@
 """
 General - not yet organized variables and functions.
 
+### CONSTANTS:
+- `TIME_UNITS` - Conversion from `str` to `int` eg. `"minute": 60`
+
 ### Functions:
 - `isPalindrome` - Checks if the `string` representation of any datatype is palindrome.
 
@@ -15,13 +18,27 @@ from xRedUtils import general
 
 import sys
 sys.dont_write_bytecode = True
-from .type_hints import SIMPLE_ANY
+from .annotations import Any
 
 __all__: tuple[str, ...] = (
+    "TIME_UNITS",
     "isPalindrome"
 )
 
-def isPalindrome(p: SIMPLE_ANY) -> bool:
+TIME_UNITS: dict[str, int] = {
+    "second": 1,
+    "minute": 60,
+    "hour": 3_600,
+    "day": 86_400,
+    "week": 604_800,
+    "month": 2_592_000,          # 30 days
+    "year": 31_536_000,          # actual 365 days
+    "decade": 315_532_800,
+    "century": 3_155_673_600,
+    "millenium": 31_556_908_800
+}
+
+def isPalindrome(p: Any) -> bool:
     """
     Checks if the `string` representation of any datatype is palindrome.
 
--- xRedUtilsAsync/general.py
+++ xRedUtilsAsync/general.py
@@ -1,6 +1,9 @@
 """
 General - not yet organized variables and async functions.
 
+### CONSTANTS:
+- `TIME_UNITS` - Conversion from `str` to `int` eg. `"minute": 60`
+
 ### Functions:
 - `isPalindrome` - Checks if the `string` representation of any datatype is palindrome.
 
@@ -15,13 +18,27 @@ from xRedUtilsAsync import general
 
 import sys
 sys.dont_write_bytecode = True
-from .type_hints import SIMPLE_ANY
+from .annotations import Any
 
 __all__: tuple[str, ...] = (
+    "TIME_UNITS",
     "isPalindrome"
 )
 
-async def isPalindrome(p: SIMPLE_ANY) -> bool:
+TIME_UNITS: dict[str, int] = {
+    "second": 1,
+    "minute": 60,
+    "hour": 3_600,
+    "day": 86_400,
+    "week": 604_800,
+    "month": 2_592_000,          # 30 days
+    "year": 31_536_000,          # actual 365 days
+    "decade": 315_532_800,
+    "century": 3_155_673_600,
+    "millenium": 31_556_908_800
+}
+
+async def isPalindrome(p: Any) -> bool:
     """
     Checks if the `string` representation of any datatype is palindrome.
 
@@ -32,4 +49,3 @@ async def isPalindrome(p: SIMPLE_ANY) -> bool:
     - `True` if its palindrome, otherwise `False`
     """
     return str(p) == str(p)[::-1]
-
